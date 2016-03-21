18e0b83cf52123437ede5e4e82d92bb6f5efba71
ThaiNV <thainv.developer@gmail.com>
2016-03-22 01:49:21 +0700

    Change config

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'aag');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'gnaht123');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
